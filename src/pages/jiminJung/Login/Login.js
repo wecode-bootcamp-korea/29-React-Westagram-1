0f55925f0e84713c6648a806dd5fdd45ef79efb7
정지민 <rindalica@gmail.com>
2022-01-12 20:55:22 +0900

    Change : Login.js, Login scss

--- src/pages/jiminJung/Login/Login.js
+++ src/pages/jiminJung/Login/Login.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import "./Login.scss";
+import './Login.scss';
 
-import Button from "../../components/Button"
-import Input from "../../components/Input"
+// import Button from '../components/Button';
+// import Input from '../components/Input';
 
 function Login() {
-    return (
-        <div className="loginWrapper">
-            <div className="login">
-                <img alt="" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" /> 
-                <div className="loginForm">
-                    <Input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-                    <Input type="password" placeholder="비밀번호" />
-                    {/* <Link to="/Main"> */}
-                    <Button name="로그인" />
-                    {/* </Link> */}
-                 </div>
-                <a href=""> 비밀번호를 잊으셨나요?</a>
-            </div>
-         </div> 
-    );
+  return (
+    <div className="loginWrapper">
+      <div className="login">
+        <img
+          className="logoImg"
+          alt=""
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+        />
+        <div className="loginForm">
+          <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+          <input type="password" placeholder="비밀번호" />
+          {/* <Link to="/Main"> */}
+          <button name="로그인" />
+          {/* </Link> */}
+        </div>
+        <a href=""> 비밀번호를 잊으셨나요?</a>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
